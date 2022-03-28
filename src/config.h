2ee63b2dc91baff3bb1fb005066f53e1b02db5ad
Bishr Ghalil <beshrghalil@protonmail.com>
2022-03-28 15:06:29 +0300

    Added config file

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#include "colors.h"
+#define FILENAME_COLOR  FCOLOR_GREEN
+#define TODO_COLOR      FCOLOR_RED
+#define FIXME_COLOR     FCOLOR_RED
+#define BUG_COLOR       FCOLOR_RED
+#define NOTE_COLOR      FCOLOR_RED
+#define NORMAL_COLOR    RESET
