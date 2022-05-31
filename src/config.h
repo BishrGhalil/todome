83ca0f18c1134103b21d43c5ae20b06fb39e8ca4
Bishr Ghalil <beshrghalil@protonmail.com>
2022-05-31 14:34:55 +0300

    config: more options more colors, prefix and suffix for printing

--- src/config.h
+++ src/config.h
@@ -1,7 +1,20 @@
 #include "colors.h"
+
+// COLORS
 #define FILENAME_COLOR  FCOLOR_GREEN
-#define TODO_COLOR      FCOLOR_RED
-#define FIXME_COLOR     FCOLOR_RED
+#define TODO_COLOR      FCOLOR_BLUE
+#define FIXME_COLOR     FCOLOR_YELLOW
 #define BUG_COLOR       FCOLOR_RED
-#define NOTE_COLOR      FCOLOR_RED
-#define NORMAL_COLOR    RESET
+#define NOTE_COLOR      FCOLOR_MAGENTA
+#define DONE_COLOR      FCOLOR_GREEN
+#define LINE_COLOR      FCOLOR_WHITE
+#define RESET_COLOR     RESET
+
+
+// PRINTING
+const char *FILE_PREFIX = "";
+const char *FILE_SUFFIX = "";
+const char *TAG_PREFIX = "  * ";
+const char *TAG_SUFFIX = "";
+const char *LINE_PREFIX = "";
+const char *LINE_SUFFIX = "";
