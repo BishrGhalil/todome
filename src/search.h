c6334b216661f45ef523e6a5cae4e6faa753a924
Bishr Ghalil <beshrghalil@protonmail.com>
2022-05-31 14:34:26 +0300

    better faster search and printing

--- src/search.h
+++ src/search.h
@@ -9,7 +9,10 @@
 #include "config.h"
 #include "errors.h"
 
-#define OVECCOUNT 3
+#define OVECCOUNT (3 * 2)
+#define MAX_TAG_LENGTH 15
+
+typedef enum { TODO, FIXME, BUG, NOTE, DONE } TAG_TYPE;
 
 typedef struct {
   char *content;
@@ -56,12 +59,50 @@ void free_mapedfile(MAPED_FILE *file) {
   free(file);
 }
 
+int is_valid_tag_level(int tag_len) {
+  if (tag_len > MAX_TAG_LENGTH)
+    return 0;
+  return 1;
+}
+
+TAG_TYPE get_tag_type(char *tag) {
+  switch (tag[0]) {
+  case 'T':
+    return TODO;
+  case 'F':
+    return FIXME;
+  case 'B':
+    return BUG;
+  case 'N':
+    return NOTE;
+  case 'D':
+    return DONE;
+  default:
+    return TODO;
+  }
+}
+
+char *get_tag_color(char *tag) {
+  switch (get_tag_type(tag)) {
+  case TODO:
+    return (char *)TODO_COLOR;
+  case FIXME:
+    return (char *)FIXME_COLOR;
+  case BUG:
+    return (char *)BUG_COLOR;
+  case NOTE:
+    return (char *)NOTE_COLOR;
+  case DONE:
+    return (char *)DONE_COLOR;
+  default:
+    return (char *)LINE_COLOR;
+  }
+}
+
 int strgrep(const char *filepath, pcre *re) {
+  int saved_errno = errno;
   int ovector[OVECCOUNT];
   int rc = 0;
-  int match_start = 0;
-  int match_end = 0;
-  int match_len = 0;
   int tmp_len = 0;
   int matched = 0;
   char *tmp;
@@ -69,28 +110,38 @@ int strgrep(const char *filepath, pcre *re) {
 
   file = map_filepath(filepath);
   if (file == NULL) {
-    errno = 0;
+    errno = saved_errno;
     return 0;
   }
 
   tmp = (char *)file->content;
-  tmp_len = strlen(tmp);
+  tmp_len = file->size;
   rc = pcre_exec(re, NULL, tmp, tmp_len, 0, 0, ovector, OVECCOUNT);
 
   if (rc > 0) {
-    printf(FILENAME_COLOR "%s\n" NORMAL_COLOR, filepath);
+    printf(FILENAME_COLOR "%s%s%s\n" RESET_COLOR, FILE_PREFIX, filepath, FILE_SUFFIX);
   }
 
   while (rc > 0) {
+    char *tag = (char *)malloc(sizeof(char) * MAX_TAG_LENGTH + 1);
     matched += 1;
-    match_start = ovector[0];
-    match_end = ovector[1];
-    match_len = match_end - match_start;
+    unsigned int line_len = ovector[1] - ovector[3];
+    unsigned int tag_len = ovector[3] - ovector[2];
 
-    printf(TODO_COLOR "%.*s" NORMAL_COLOR, match_len, &tmp[match_start]);
+    if (!is_valid_tag_level(tag_len)) {
+      tag_len = MAX_TAG_LENGTH;
+    }
+    strncpy(tag, &tmp[ovector[2]], tag_len);
+    tag[tag_len] = '\0';
 
-    int startoffset = println(&tmp[match_end]) + match_start;
-    rc = pcre_exec(re, NULL, tmp, tmp_len, startoffset, 0, ovector, OVECCOUNT);
+    // PRINT TAG
+    printf("%s%s%s%s%s", TAG_PREFIX, get_tag_color(tag), tag, RESET_COLOR, TAG_SUFFIX);
+
+    // PRINT TODO_BODY
+    printf(LINE_COLOR "%s%.*s%s\n" RESET_COLOR, LINE_PREFIX, line_len, &tmp[ovector[3]], LINE_SUFFIX);
+
+    rc = pcre_exec(re, NULL, tmp, tmp_len, ovector[1], 0, ovector, OVECCOUNT);
+    free(tag);
   }
 
   free_mapedfile(file);
