5bbc1b42af3c136b1d89bb2831445bf0b74a8ae8
Bishr Ghalil <beshrghalil@protonmail.com>
2022-06-01 08:16:36 +0300

    dna_string: changed the string library name

---
+++ src/dna_string.c
@@ -0,0 +1,78 @@
+/* todome - search for TODOs FIXMEs BUGs and NOTEs ... in source code
+   Copyright (C) 2021-2021 Bishr Ghalil.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bishr Ghalil */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dna_string.h"
+
+STRING *string_new(char *data, size_t n) {
+  STRING *str = (STRING *)malloc(sizeof(*str));
+  if (n < DEFAULT_STRING_SIZE) {
+    str->size = DEFAULT_STRING_SIZE;
+  } else {
+    str->size = DEFAULT_STRING_SIZE + n;
+  }
+  str->data = (char *)malloc(sizeof(char) * str->size);
+  str->len = n;
+  while (n--) {
+    str->data[n] = data[n];
+  }
+
+  str->data[str->len] = '\0';
+  return str;
+}
+
+void string_printinfo(STRING *str) {
+  printf("(%zu), %s\n", str->len, str->data);
+}
+
+STRING *string_nresize(STRING *str, size_t newsize) {
+  str->size = newsize;
+  str->data = (char *)realloc(str->data, newsize);
+  return str;
+}
+
+STRING *string_resize(STRING *str) {
+  return string_nresize(str, str->size + DEFAULT_STRING_SIZE);
+}
+
+void string_free(STRING *str) {
+  free(str->data);
+  free(str);
+}
+
+void string_concat(STRING *dest, STRING *src) {
+  dest->size += src->size;
+  dest->len += src->len;
+  dest->data = (char *)realloc(dest->data, dest->size);
+  strncat(dest->data, src->data, src->len);
+}
+
+void string_charr_concat(STRING *dest, char *src, size_t n) {
+  if (dest->size <= dest->len + n) {
+    string_nresize(dest, dest->size + n);
+  }
+
+  strncat(dest->data, src, n);
+}
+
+char *string_data(STRING *str) {
+  return str->data;
+}
---
+++ src/dna_string.h
@@ -0,0 +1,42 @@
+/* todome - search for TODOs FIXMEs BUGs and NOTEs ... in source code
+   Copyright (C) 2021-2021 Bishr Ghalil.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bishr Ghalil */
+
+
+#ifndef STRING_TYPE_H_
+#define STRING_TYPE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_STRING_SIZE 128
+
+typedef struct {
+  char *data;
+  size_t len;
+  size_t size;
+} STRING;
+
+STRING *string_new(char *data, size_t n);
+
+void string_printinfo(STRING *str);
+void string_free(STRING *str);
+void string_concat(STRING *dest, STRING *src);
+void string_charr_concat(STRING *dest, char *src, size_t n);
+char *string_data(STRING *str);
+#endif
